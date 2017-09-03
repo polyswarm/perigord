84531ff65fe0e5148ecf2cb78c2d5fd8b4cff3dc
Maxwell Koo <mjkoo90@gmail.com>
2017-09-03 18:42:58 -0400

    Testing start

---
+++ templates/test/test.go
@@ -0,0 +1,28 @@
+package tests
+
+import (
+	"io"
+	"testing"
+
+	. "gopkg.in/check.v1"
+
+	"github.com/swarmdotmarket/perigord/testing"
+)
+
+type suite struct{}
+
+var _ = Suite(&suite{})
+
+func (s *suite) SetUpTest(c *C) {
+	testing.SetUpTest()
+}
+
+func (s *suite) TearDownTest(c *C) {
+	testing.TearDownTest()
+}
+
+// USER TESTS GO HERE
+
+func (s *suite) TestFoo(c *C) {
+	c.Succeed()
+}

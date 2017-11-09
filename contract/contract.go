6fad2c01e26b7e526137c621db7e50a8f0347e77
Maxwell Koo <mjkoo90@gmail.com>
2017-11-09 17:51:32 -0500

    Fix more rebranding breakage

--- contract/contract.go
+++ contract/contract.go
@@ -20,7 +20,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 
-	"github.com/swarmdotmarket/perigord/migration"
+	"github.com/polyswarm/perigord/migration"
 )
 
 type ContractDeployer interface {
